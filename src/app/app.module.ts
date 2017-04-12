4b212d8364f01564c570307acbb320fa360f57c3
Rico <richardthomaswhitfield@gmail.com>
2017-04-12 15:57:53 -0500

    updated app module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-import { AppComponent }  from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
-
+export class AppModule {
+}
